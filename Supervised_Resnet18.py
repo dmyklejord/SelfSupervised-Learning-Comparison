b7c387cdf0d7c7517a4bc8e1c13d2ccd92057832
dmyklejord <125533929+dmyklejord@users.noreply.github.com>
2023-05-04 19:30:28 -0500

    Add files via upload

---
+++ Supervised_Resnet18.py
@@ -0,0 +1,165 @@
+import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import datasets, models, transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import os
+import helper_evaluate
+import helper_data
+
+# Set to run on mps if available (i.e. Apple's GPU).
+# mps is a new pytorch feature, so we check that
+# it's also available with the user's pytorch install.
+DEVICE = 'mps' if hasattr(torch.backends, "mps") and torch.backends.mps.is_available() else 'cpu'
+
+# Hyperparameters
+RANDOM_SEED = 123
+LEARNING_RATE = 0.0001
+BATCH_SIZE = 128
+NUM_EPOCHS = 100
+helper_evaluate.set_deterministic
+helper_evaluate.set_all_seeds(RANDOM_SEED)
+
+data_location = ('/Users/duanemyklejord/Documents/Capstone/PlantAutomatedScripts/data')
+# train_loader, test_loader = helper_data.get_dataloaders(data_location, batch_size=BATCH_SIZE)
+
+for LEARNING_RATE in [0.0001, 0.0003, 0.01, 0.03, 0.06, 0.1, 1, 10]:
+   for NUM_DATAPOINTS in [5, 10, 20, 50, 100, 200, 250]:
+
+    BATCH_SIZE = NUM_DATAPOINTS
+    if BATCH_SIZE > 128:
+        BATCH_SIZE = 128
+
+    # Use all the data:
+    if NUM_DATAPOINTS == 250:
+        model_name = f'Supervised_Resnet18_{LEARNING_RATE}LR_SimCLRAug'
+        train_loader, test_loader = helper_data.get_dataloaders(data_location, batch_size=BATCH_SIZE)
+    else:
+        model_name = f'Supervised_Resnet18_{LEARNING_RATE}LR_{NUM_DATAPOINTS}DP_SimCLRAug'
+        train_loader, test_loader = helper_data.get_dataloaders_reduced_data(data_location, batch_size=BATCH_SIZE, num_datapoints=NUM_DATAPOINTS)
+    class_names = [f.name for f in os.scandir(data_location) if f.is_dir()]
+
+    # Use a pre-trained CNN model for feature extraction
+    model = models.resnet18(weights='DEFAULT')
+    num_features = model.fc.in_features
+    model.fc = nn.Linear(num_features, len(class_names))
+    model.to(DEVICE)
+
+    # Define the loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+
+
+    comp_log_dict = {'final_epoch_loss':[],
+                    'final_batch_loss': [],
+                    'final_accuracy': [],
+                    'training_time': [],
+                    'model': model.__class__.__name__,
+                    'learning_rate': LEARNING_RATE,
+                    'batch_size': BATCH_SIZE,
+                    'num_epochs': NUM_EPOCHS,
+                    'num_datapoints': NUM_DATAPOINTS,
+                    'optimizer_type': optimizer.__class__.__name__
+                    }
+
+    try:
+        os.mkdir(model_name)
+    except:
+        pass
+
+    os.chdir('/Users/duanemyklejord/Documents/Capstone/PlantAutomatedScripts/'+model_name)
+
+
+    # If you have an already trained model, it can be loaded. Training steps can then be skipped.
+    # model.load_state_dict(torch.load(f'{model_name}.pt')) # to load a pre-trained model
+
+    # Fine-tune the CNN model on the given dataset
+    start_time = time.time()
+    train_loss_per_epoch = []
+    for epoch in range(NUM_EPOCHS):
+        epoch_loss = 0
+        print(f'Current model: {model_name}')
+        for batch_idx, (inputs, _, labels) in enumerate(train_loader):
+            inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
+            
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            epoch_loss += loss.item()
+                        
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if not batch_idx % 4:
+                print('Epoch: %03d/%03d | Batch %04d/%04d | Loss: %.4f'
+                        % (epoch+1, NUM_EPOCHS, batch_idx,
+                            len(train_loader), loss))
+        
+        train_loss_per_epoch.append(epoch_loss)
+        print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))    
+    print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+    comp_log_dict['training_time'] = ((time.time() - start_time)/60)
+    torch.save(model.state_dict(), model_name+'.pt')
+
+    # Plot the epoch losses
+    plt.plot(np.linspace(1, NUM_EPOCHS, NUM_EPOCHS), train_loss_per_epoch)
+    plt.savefig(model_name+'_EpochLossPlot.png')
+
+    # Evaluate the model on the test dataset
+    print('Evaluating the model on the test dataset...')
+    correct, total = 0, 0
+    pred_labels, labels = [], []
+    model.eval().to('cpu')
+    with torch.no_grad():
+        for images, _, batch_labels in test_loader:
+
+            outputs = model(images)
+            _, batch_preds = torch.max(outputs.data, 1)
+            
+            total += batch_labels.size(0)
+            correct += (batch_preds == batch_labels).sum().item()
+            
+            pred_labels.extend(batch_preds.numpy())
+            labels.extend(batch_labels.numpy())
+
+
+    accuracy = correct / total
+    print(f'Accuracy of the network on the test images: {accuracy}')
+    comp_log_dict['final_accuracy'] = accuracy
+
+    w = csv.writer(open(model_name+'_Meta.csv', "w"))
+    for key, val in comp_log_dict.items():
+        w.writerow([key, val])
+
+    # Calculate classification statistics
+    from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+    import matplotlib.pyplot as plt
+
+    confusion = confusion_matrix(labels, pred_labels)
+    report = classification_report(labels, pred_labels)
+
+    # Print the classification statistics
+    print('Confusion Matrix:')
+    print(confusion)
+    print('\nClassification Report:')
+    print(report)
+
+    helper_evaluate.visualize_confusion_matrix(confusion, accuracy, class_names, model_name)
+
+    # plt.rcParams["figure.autolayout"]= True # to make sure the labels don't get cut off
+    # display = ConfusionMatrixDisplay(confusion, display_labels=class_names)
+    # fig, ax = plt.subplots(figsize=(10, 10))
+    # display.plot(ax=ax, xticks_rotation=45)
+    # # plt.xticks(rotation = 45, ha='right') # Rotates X-Axis Ticks by 45-degrees
+    # plt.show()
+    # plt.savefig(model_name+'_ConfusionMatrix.png')
+
+    os.chdir('/Users/duanemyklejord/Documents/Capstone/PlantAutomatedScripts/')
---
+++ helper_data.py
@@ -0,0 +1,237 @@
+import torch
+import torchvision
+
+from torch.utils.data import DataLoader
+from torch.utils.data import SubsetRandomSampler
+from torch.utils.data import sampler
+from torch.utils.data import random_split
+from torchvision.datasets import CIFAR10
+from PIL import Image
+
+import numpy as np
+
+
+# Set image augmentations. These are the default SimCLR augmentations.
+class simclr_transforms():
+    size = 224
+    s = 1
+    color_jitter = torchvision.transforms.ColorJitter(
+                0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+    train = torchvision.transforms.Compose([
+                    torchvision.transforms.RandomResizedCrop(size=size),
+                    torchvision.transforms.RandomHorizontalFlip(),  # with 0.5 probability
+                    torchvision.transforms.RandomApply([color_jitter], p=0.8),
+                    torchvision.transforms.RandomGrayscale(p=0.2),
+                    torchvision.transforms.ToTensor(),
+    ])
+    test= torchvision.transforms.Compose([
+                    torchvision.transforms.Resize(size=(size, size)),
+                    torchvision.transforms.ToTensor(),
+    ])
+
+class ReducedTransforms():
+    # Define the transformations to be applied to the input images. resnet-18 needs a 224x224 image.
+    train = torchvision.transforms.Compose([
+                    torchvision.transforms.RandomResizedCrop((224,224), scale=(0.8, 1.0)),
+                    torchvision.transforms.RandomHorizontalFlip(),
+                    torchvision.transforms.RandomRotation(90),
+                    torchvision.transforms.ToTensor()
+    ])
+
+    test = torchvision.transforms.Compose([
+                    torchvision.transforms.Resize((224, 224)),
+                    torchvision.transforms.ToTensor()
+    ])
+
+# # Define the transformations to be applied to the input images. resnet-18 needs a 224x224 image.
+# transform = transforms.Compose([
+#     transforms.RandomResizedCrop((224,224), scale=(0.4, 1.0)),
+#     transforms.RandomHorizontalFlip(),
+#     transforms.RandomRotation(90),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.5], std=[0.5])
+# ]) 
+
+
+class CIFAR10Pair(CIFAR10):
+    """
+    From: https://github.com/leftthomas/SimCLR/blob/master/utils.py
+    A stochastic data augmentation module that transforms
+    any given data example randomly
+    resulting in two correlated views of the same example,
+    denoted x ̃i and x ̃j, which we consider as a positive pair.
+    """
+
+    def __getitem__(self, index):
+        img, target = self.data[index], self.targets[index]
+        img = Image.fromarray(img)
+
+        if self.transform is not None:
+            pos_1 = self.transform(img)
+            pos_2 = self.transform(img)
+
+        # Transform for the knn feature bank function:
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return pos_1, pos_2, target
+
+    def __len__(self):
+        return len(self.data)
+
+
+def get_dataloaders_cifar10(
+    train_transforms=simclr_transforms.train,
+    test_transforms=simclr_transforms.test,
+    batch_size=128,
+    num_workers=0):
+
+    train_simclr_dataset = CIFAR10Pair(
+        root='Cifar10_data',
+        download=True,
+        train=True,
+        transform=train_transforms)
+
+    test_simclr_dataset = CIFAR10Pair(
+        root='Cifar10_data',
+        download=True,
+        train=False,
+        transform=test_transforms)
+
+    train_loader = DataLoader(
+        train_simclr_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+        drop_last=True)
+
+    test_loader = DataLoader(
+        test_simclr_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True,
+        drop_last=True)
+
+    return train_loader, test_loader
+
+
+class ImagePair(torchvision.datasets.ImageFolder):
+    """
+    Returns the same image transformed two ways.
+    Modified from: https://github.com/pytorch/vision/blob/main/torchvision/datasets/folder.py
+    """
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample1 ,sample2, target) where target is class_index of the target class.
+            sample1 and sample2 are augmented/transformed versions of sample (sample==image).
+        """
+        path, target = self.samples[index]
+        sample = self.loader(path)
+
+        if self.transform is not None:
+            sample1 = self.transform(sample)
+            sample2 = self.transform(sample)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample1, sample2, target
+
+    def __len__(self):
+        return len(self.samples)
+
+
+def get_dataloaders(
+    dir='data',
+    train_transforms=ReducedTransforms.train,
+    test_transforms=ReducedTransforms.test,
+    batch_size=128,
+    test_fraction=0.20,
+    num_workers=0):
+
+    # Getting transformed image pairs of the seedling data:
+    train_dataset = ImagePair(root=dir, transform=train_transforms)
+    test_dataset = ImagePair(root=dir, transform=test_transforms)
+
+    # Splitting into training and testing datasets
+    # using the test_fraction split:
+    length_data = len(train_dataset)
+    dataset_indices = range(length_data)
+    length_test = int(test_fraction * length_data)
+    train_sampler, test_sampler = random_split(dataset_indices, [length_data-length_test, length_test], generator=torch.Generator().manual_seed(123))
+
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=batch_size,
+                              num_workers=num_workers,
+                              drop_last=True,
+                              sampler=train_sampler)
+
+
+    test_loader = DataLoader(dataset=test_dataset,
+                             batch_size=batch_size,
+                             num_workers=num_workers,
+                             sampler=test_sampler)
+
+    return train_loader, test_loader
+
+def get_dataloaders_reduced_data(
+    dir='data',
+    train_transforms=ReducedTransforms.train,
+    test_transforms=ReducedTransforms.test,
+    batch_size=128,
+    test_fraction=0.20,
+    num_datapoints=20,
+    num_workers=0):
+
+    # Getting transformed image pairs of the seedling data:
+    train_dataset = ImagePair(root=dir, transform=train_transforms)
+    test_dataset = ImagePair(root=dir, transform=test_transforms)
+
+    # Splitting into training and testing datasets
+    # using the test_fraction split:
+    length_data = len(train_dataset)
+    dataset_indices = range(length_data)
+    length_test = int(test_fraction * length_data)
+    
+    # The datapoint indices of the original dataset are randomly split into testing and training. 
+    train_sampler, test_sampler = random_split(dataset_indices, [length_data-length_test, length_test], generator=torch.Generator().manual_seed(123))
+
+    # Getting num_datapoints from each class:
+    train_idxs = []
+    for class_idx in range(len(np.unique(train_dataset.targets))):
+        
+        # Gets the indices of the all the samples of a certain class
+        # i.e. all the indicies of the first class
+        class_idxs = np.where(np.array(train_dataset.targets) == class_idx)[0]
+
+        # Makes sure that those indices belong to the training set
+        random_class_idxs = []
+        for idx in class_idxs:
+            if idx in train_sampler.indices:
+                random_class_idxs.append(idx)
+
+        # Randomly pick out num_datapoints from the class
+        train_idxs = np.concatenate((train_idxs, np.random.choice(random_class_idxs, num_datapoints, replace=False)))
+    
+    reduced_train_dataset = torch.utils.data.Subset(train_dataset, train_idxs.astype(int))
+    reduced_test_dataset = torch.utils.data.Subset(test_dataset, test_sampler.indices)
+
+    train_loader = DataLoader(dataset=reduced_train_dataset,
+                              batch_size=batch_size,
+                              num_workers=num_workers,
+                              shuffle=True,
+                              drop_last=True)
+
+
+    test_loader = DataLoader(dataset=reduced_test_dataset,
+                             batch_size=batch_size,
+                             num_workers=num_workers,
+                             drop_last=False)
+
+    return train_loader, test_loader
